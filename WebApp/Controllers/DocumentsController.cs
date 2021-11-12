0d303ffd9c14a259e77e054b37d9e589ccc1cdee
JohNy <come.as@you.are>
2021-11-12 09:26:59 +0300

    Добавил контроллер для регистрации документов

---
+++ WebApp/Controllers/DocumentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class DocumentsController : ApiController
+    {
+        [HttpPost]
+        [Route("docs/register")]
+        public HttpResponseMessage RegisterNewDocument(string document)
+        {
+            
+            return Request.CreateResponse(HttpStatusCode.OK, "Документ зарегистрирован");
+        }
+    }
+}
